56572ab42ff66e98df3e98d9b4d76a5f0464ad32
AAKON <FAISALAKON444@GMAIL.COM>
2020-07-21 13:47:17 +0600

    db added

--- aqualink/settings.py
+++ aqualink/settings.py
@@ -85,20 +85,31 @@ WSGI_APPLICATION = 'aqualink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'aqualink3',
+#         'USER':'root',
+#         'PASSWORD':'',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aqualink3',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aqualink',
+        'USER': 'aqualink_dbadmin',
+        'PASSWORD': 'aqualink@321',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-
 }
 
 
