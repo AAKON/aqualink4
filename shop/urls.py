053a4f04a5d4357e8b9ccc4298cc1f51f09247bf
AAKON <faisalakon444@gmail.com>
2020-09-27 00:57:52 +0600

    fixing pdf problem

--- shop/urls.py
+++ shop/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('add_to_cart/<int:pk>',views.add_to_cart,name='add_to_cart'),
     path('remove_from_cart/<int:pk>',views.remove_from_cart,name='remove_from_cart'),
     path('cart/',views.cart,name = 'cart'),
-    path('order/',views.order,name = 'order'),
-    path('quotation/',views.quotation,name = 'quotation')
+    path('order/',views.order,name = 'order')
 
 
 ]
